7564c1be22bac82d880c4e0b02fae1716176568b
Pedro Alberto Veber Berna <pedro.veber@outlook.com>
2023-02-10 13:01:29 -0300

    sign up page created

--- resources/views/auth/create.blade.php
+++ resources/views/auth/create.blade.php
@@ -1,3 +1,24 @@
 <x-auth title="Register">
-    <h1 class="font-sans">Resident Evil 4</h1>
+  <form class="w-full max-w-[600px] mx-auto flex flex-col gap-2">
+    <div class="form-group">
+      <label for="name" class="label-default">Name</label>
+      <input type="text" name="name" id="name" placeholder="Name..." class="input-default">
+    </div>
+    <div class="form-group">
+      <label for="email" class="label-default">Email</label>
+      <input type="text" name="email" id="email" placeholder="E-mail..." class="input-default">
+    </div>
+    <div class="form-group">
+      <label for="password" class="label-default">Password</label>
+      <input type="password" name="password" id="password" placeholder="Password..." class="input-default">
+    </div>
+    <div class="form-group">
+      <label for="confirmpassword" class="label-default">Confirm Password</label>
+      <input type="password" name="confirmpassword" id="confirmpassword" placeholder="Confirm Password..." class="input-default">
+    </div>
+    <button type="submit" class="btn-default">
+      Sing Up
+    </button>
+  </form>
+  <p class="text-gray-200 block w-full max-w-[600px] mx-auto">Do you already have an account? <a href="#" class="link-default">Click here</a></p>
 </x-auth>
--- resources/views/components/auth.blade.php
+++ resources/views/components/auth.blade.php
@@ -10,8 +10,15 @@
   
   <title>RE 4 - {{ $title }}</title>
 </head>
-
-<body>
-  {{ $slot }}
+<body class="bg-black">
+  <header class="flex justify-center items-center py-2 w-full h-44 sm:h-32 border-b-gray-500 border-b-2">
+    <img src="/img/resident-evil-4-logo.png" alt="Resident Evil 4" class="w-72 sm:w-52">
+  </header>
+  <main class="min-h-screen px-8 py-12 max-w-[1400px] mx-auto">
+    {{ $slot }}
+  </main>
+  <footer class="font-upper h-28 flex items-center justify-center text-gray-200 text-xl border-t-2 border-t-gray-500">
+    Capcom - &copy; 2023
+  </footer>
 </body>
 </html>
